6a84ab9bf6c2634b99fe783d59d9c8fddc56f62e
Stephen Elcombe <selcombe@hotmail.co.uk>
2019-06-05 07:36:22 +0000

    Amended index file due to non compliance with HTML validator

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
   <head>
     <title>Your Next Holiday Destination</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
     <meta charset="utf-8">
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
@@ -262,7 +262,7 @@ width: 840px;
   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Poppins:300,400,500,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans|Poppins" rel="stylesheet">
 
   <!-- Bootstrap CSS File -->
   <link href="assets/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -299,7 +299,7 @@ width: 840px;
   <!-- Hero -->
   <section id="hero">
     <div class="hero-container">
-      <h1>Welcome to Your Next Holiday Destination</h1>
+      <h2>Welcome to Your Next Holiday Destination</h2>
       <h2>We find you activities, accommodation and much more around the area you'll be staying in</h2>
       <a href="#search" class="btn-get-started">Search Now</a>
     </div>
@@ -321,105 +321,103 @@ width: 840px;
 
     <div id="findhotels" class="findhotels">
     
-    <form  method="POST" action="index.php#map" >
+    <form  method="POST" action="index.php#findhotels" >
   
-   
-    <p><FONT COLOR=black><h2>What are you looking for :</h2></p>
+    <br><br><h3>What are you looking for :</h3><br>
       <input id="tags" name="tags" style="width:200px;" value="<?php echo $_POST['tags'];?>" type="hidden">
-<select name="tags">
-  <option value="accounting">Accounting</option>
-  <option value="airport">Airport</option>
-  <option value="amusement_park">Amusement Park</option>
-  <option value="aquarium">Aquarium</option>
-  <option value="art_gallery">Art gallery</option>
-  <option value="atm">ATM</option>
-  <option value="bakery">Bakery</option>
-  <option value="bank">Bank</option>
-  <option value="bar">Bar</option>
-  <option value="beauty_salon">Beauty Salon</option>
-  <option value="bicycle_store">Bicycle Store</option>
-  <option value="book_store">Book Store</option>
-  <option value="bowling_alley">Bowling Alley</option>
-  <option value="bus_station">Bus Station</option>
-  <option value="cafe">Cafe</option>
-  <option value="campground">Campground</option>
-  <option value="car_dealer">Car Dealer</option>
-  <option value="car_rental">Car Rental</option>
-  <option value="car_repair">Car Repair</option>
-  <option value="car_wash">Car Wash</option>
-  <option value="casino">Casino</option>
-  <option value="cemetery">Cemetery</option>
-  <option value="church">Church</option>
-  <option value="city_hall">City Hall</option>
-  <option value="clothing_store">Clothing Store</option>
-  <option value="convenience_store">Convenience Store</option>
-  <option value="courthouse">Courthouse</option>
-  <option value="dentist">Dentist</option>
-  <option value="department_store">Department Store</option>
-  <option value="doctor">Doctor</option>
-  <option value="electrician">Electrician</option>
-  <option value="electronics_store">Electronics Store</option>
-  <option value="embassy">Embassy</option>
-  <option value="fire_station">Fire Station</option>
-  <option value="florist">Florist</option>
-  <option value="funeral_home">Funeral Home</option>
-  <option value="furniture_store">Furniture Store</option>
-  <option value="gas_station">Gas Station</option>
-  <option value="gym">Gym</option>
-  <option value="hair_care">Hair Care</option>
-  <option value="hardware_store">Hardware Store</option>
-  <option value="hindu_temple">Hindu Temple</option>
-  <option value="home_goods_store">Home Goods Store</option>
-  <option value="hospital">Hospital</option>
-  <option value="insurance_agency">Insurance Agency</option>
-  <option value="jewelry_store">Jewelry Store</option>
-  <option value="laundry">Laundry</option>
-  <option value="lawyer">Lawyer</option>
-  <option value="library">Library</option>
-  <option value="liquor_store">Liquor Store</option>
-  <option value="local_government_office">Local Government Office</option>
-  <option value="locksmith">Locksmith</option>
-  <option value="lodging">Lodging</option>
-  <option value="meal_delivery">Meal Delivery</option>
-  <option value="meal_takeaway">Meal Takeaway</option>
-  <option value="mosque">Mosque</option>
-  <option value="movie_rental">Movie Rental</option>
-  <option value="movie_theater">Movie Theater</option>
-  <option value="moving_company">Moving Company</option>
-  <option value="museum">Museum</option>
-  <option value="night_club">Night Club</option>
-  <option value="painter">Painter</option>
-  <option value="park">Park</option>
-  <option value="parking">Parking</option>
-  <option value="pet_store">Pet Store</option>
-  <option value="pharmacy">Pharmacy</option>
-  <option value="physiotherapist">Physiotherapist</option>
-  <option value="plumber">Plumber</option>
-  <option value="police">Police</option>
-  <option value="post_office">Post Office</option>
-  <option value="real_estate_agency">Real Estate Agency</option>
-  <option value="restaurant">Restaurant</option>
-  <option value="roofing_contractor">Roofing Contractor</option>
-  <option value="rv_park">RV Park</option>
-  <option value="school">School</option>
-  <option value="shoe_store">Shoe Store</option>
-  <option value="shopping_mall">Shopping Mall</option>
-  <option value="spa">Spa</option>
-  <option value="stadium">Stadium</option>
-  <option value="storage">Storage</option>
-  <option value="store">Store</option>
-  <option value="subway_station">Subway Station</option>
-  <option value="supermarket">Supermarket</option>
-  <option value="synagogue">Synagogue</option>
-  <option value="taxi_stand">Taxi Stand</option>
-  <option value="train_station">Train Station</option>
-  <option value="transit_station">Transit Station</option>
-  <option value="travel_agency">Travel Agency</option>
-  <option value="veterinary_care">Veterinary Care</option>
-  <option value="zoo">Zoo</option>
-</select>
+      <select name="tags">
+        <option value="accounting">Accounting</option>
+        <option value="airport">Airport</option>
+        <option value="amusement_park">Amusement Park</option>
+        <option value="aquarium">Aquarium</option>
+        <option value="art_gallery">Art gallery</option>
+        <option value="atm">ATM</option>
+        <option value="bakery">Bakery</option>
+        <option value="bank">Bank</option>
+        <option value="bar">Bar</option>
+        <option value="beauty_salon">Beauty Salon</option>
+        <option value="bicycle_store">Bicycle Store</option>
+        <option value="book_store">Book Store</option>
+        <option value="bowling_alley">Bowling Alley</option>
+        <option value="bus_station">Bus Station</option>
+        <option value="cafe">Cafe</option>
+        <option value="campground">Campground</option>
+        <option value="car_dealer">Car Dealer</option>
+        <option value="car_rental">Car Rental</option>
+        <option value="car_repair">Car Repair</option>
+        <option value="car_wash">Car Wash</option>
+        <option value="casino">Casino</option>
+        <option value="cemetery">Cemetery</option>
+        <option value="church">Church</option>
+        <option value="city_hall">City Hall</option>
+        <option value="clothing_store">Clothing Store</option>
+        <option value="convenience_store">Convenience Store</option>
+        <option value="courthouse">Courthouse</option>
+        <option value="dentist">Dentist</option>
+        <option value="department_store">Department Store</option>
+        <option value="doctor">Doctor</option>
+        <option value="electrician">Electrician</option>
+        <option value="electronics_store">Electronics Store</option>
+        <option value="embassy">Embassy</option>
+        <option value="fire_station">Fire Station</option>
+        <option value="florist">Florist</option>
+        <option value="funeral_home">Funeral Home</option>
+        <option value="furniture_store">Furniture Store</option>
+        <option value="gas_station">Gas Station</option>
+        <option value="gym">Gym</option>
+        <option value="hair_care">Hair Care</option>
+        <option value="hardware_store">Hardware Store</option>
+        <option value="hindu_temple">Hindu Temple</option>
+        <option value="home_goods_store">Home Goods Store</option>
+        <option value="hospital">Hospital</option>
+        <option value="insurance_agency">Insurance Agency</option>
+        <option value="jewelry_store">Jewelry Store</option>
+        <option value="laundry">Laundry</option>
+        <option value="lawyer">Lawyer</option>
+        <option value="library">Library</option>
+        <option value="liquor_store">Liquor Store</option>
+        <option value="local_government_office">Local Government Office</option>
+        <option value="locksmith">Locksmith</option>
+        <option value="lodging">Lodging</option>
+        <option value="meal_delivery">Meal Delivery</option>
+        <option value="meal_takeaway">Meal Takeaway</option>
+        <option value="mosque">Mosque</option>
+        <option value="movie_rental">Movie Rental</option>
+        <option value="movie_theater">Movie Theater</option>
+        <option value="moving_company">Moving Company</option>
+        <option value="museum">Museum</option>
+        <option value="night_club">Night Club</option>
+        <option value="painter">Painter</option>
+        <option value="park">Park</option>
+        <option value="parking">Parking</option>
+        <option value="pet_store">Pet Store</option>
+        <option value="pharmacy">Pharmacy</option>
+        <option value="physiotherapist">Physiotherapist</option>
+        <option value="plumber">Plumber</option>
+        <option value="police">Police</option>
+        <option value="post_office">Post Office</option>
+        <option value="real_estate_agency">Real Estate Agency</option>
+        <option value="restaurant">Restaurant</option>
+        <option value="roofing_contractor">Roofing Contractor</option>
+        <option value="rv_park">RV Park</option>
+        <option value="school">School</option>
+        <option value="shoe_store">Shoe Store</option>
+        <option value="shopping_mall">Shopping Mall</option>
+        <option value="spa">Spa</option>
+        <option value="stadium">Stadium</option>
+        <option value="storage">Storage</option>
+        <option value="store">Store</option>
+        <option value="subway_station">Subway Station</option>
+        <option value="supermarket">Supermarket</option>
+        <option value="synagogue">Synagogue</option>
+        <option value="taxi_stand">Taxi Stand</option>
+        <option value="train_station">Train Station</option>
+        <option value="transit_station">Transit Station</option>
+        <option value="travel_agency">Travel Agency</option>
+        <option value="veterinary_care">Veterinary Care</option>
+        <option value="zoo">Zoo</option>
+      </select>
      <input type="submit" name="envoi" value="Search"  id="Submit" /> 
-     
     
     </form>
     <form  method="POST" action="https://your-next-holiday-destination-sae2018.c9users.io/index.php#findhotels" >
